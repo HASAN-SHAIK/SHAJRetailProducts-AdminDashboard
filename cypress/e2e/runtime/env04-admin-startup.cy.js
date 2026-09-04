b35d09b31b4218cd610c28e74cdb2b6de1efbd57
HASAN-SHAIK <59140951+HASAN-SHAIK@users.noreply.github.com>
2026-09-04 14:13:39 +0530

    test: tighten ENV-04 startup acceptance

--- cypress/e2e/runtime/env04-admin-startup.cy.js
+++ cypress/e2e/runtime/env04-admin-startup.cy.js
@@ -3,9 +3,8 @@ describe('ENV-04 AdminDashboard startup', () => {
     cy.visit('/');
     cy.location('pathname').should('eq', '/admin/login');
     cy.contains('Admin Login').should('be.visible');
-    cy.findByLabelText ? null : null;
-    cy.get('input').filter('[type="email"]').should('be.visible');
-    cy.get('input').filter('[type="password"]').should('be.visible');
+    cy.get('input[type="email"]').should('be.visible');
+    cy.get('input[type="password"]').should('be.visible');
     cy.contains('button', 'Login').should('be.visible');
   });
 });
